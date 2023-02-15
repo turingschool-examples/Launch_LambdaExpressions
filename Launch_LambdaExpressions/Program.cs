6583b561d2da821e14c697eb3195a3da954b5243
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2023-02-15 09:32:49 -0700

    Add project files.

---
+++ Launch_LambdaExpressions/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Square(5));
+        Console.WriteLine(Double(5));
+    }
+
+    static int Square(int number)
+    {
+        return number * number;
+    }
+
+    static int Double(int number)
+    {
+        return number * 2;
+    }
+}
